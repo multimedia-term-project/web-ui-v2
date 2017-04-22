94cee0c4531f02105934e2cf3c49c7943aad694d
Denver Huynh <dhuynh@DenverHuynh.local>
2017-04-22 17:21:36 -0400

    added ngmodel for fields in register and defined onClick for the button

--- denvr-web-ui/src/app/app.component.ts
+++ denvr-web-ui/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-  template:'<router-outlet></router-outlet> ',
-=======
-  template:' <router-outlet></router-outlet> ',
->>>>>>> 59c2c6561f347054ed3c5e09d7f12b3cb949bcff
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
