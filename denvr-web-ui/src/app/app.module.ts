f31668c726e1da572d6a39d6d931d5bfab17f67e
Kyle Fetterman <kfetterm@iupui.edu>
2017-04-22 16:10:03 -0400

    router

--- denvr-web-ui/src/app/app.module.ts
+++ denvr-web-ui/src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import {registerComponent} from './register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: 'register', component: registerComponent },
+];
+
+
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    registerComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+              registerComponent]
 })
+
 export class AppModule { }
