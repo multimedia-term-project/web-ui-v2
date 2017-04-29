a9d4553b8d2365d5503b225ddffa9849fb98a5fd
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-04-29 15:13:30 -0400

    new changes

--- denvr-web-ui/src/app/register.service.ts
+++ denvr-web-ui/src/app/register.service.ts
@@ -18,7 +18,11 @@ import {register} from './register'
      constructor (private http: Http) {}
 
      create(user): Observable<register> {
-       let headers = new Headers({ 'Content-Type': 'application/json' });
+       let headers = new Headers({ 'Content-Type': 'application/json',
+         'Access-Control-Allow-Origin': "*",
+         'Access-Control-Allow-Credentials':'true',
+         'Access-Control-Allow-Methods':'GET, HEA, OPTIONS,POST,PUT',
+         'Access-Control-Allow-Headers':'Access-Control-Allow-Headers,  Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'});
        let options = new RequestOptions({ headers: headers });
        console.log(this.userUrl);
     return this.http.post(this.userUrl, { name }, options)
