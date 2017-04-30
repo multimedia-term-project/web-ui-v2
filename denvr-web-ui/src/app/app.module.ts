520e9234e8fc38959827a4daeb4fcea2baa4c173
Denver Huynh <dhuynh@DenverHuynh.local>
2017-04-30 01:17:30 -0400

    separate home page for map and faces view

--- denvr-web-ui/src/app/app.module.ts
+++ denvr-web-ui/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {registerComponent} from './register.component';
 import {loginComponent} from './login.component';
 import {homeComponent} from './home.component';
+import {homeMapComponent} from './homeMap.component';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AgmCoreModule } from 'angular2-google-maps/core';
@@ -16,6 +17,7 @@ const appRoutes: Routes = [
   { path: 'register', component: registerComponent },
   { path: 'login', component: loginComponent },
   { path: 'home', component: homeComponent },
+  { path: 'homeMap', component: homeMapComponent },
 ];
 
 
@@ -28,6 +30,7 @@ const appRoutes: Routes = [
     registerComponent,
     loginComponent,
     homeComponent,
+    homeMapComponent,
   ],
   imports: [
     BrowserModule,
