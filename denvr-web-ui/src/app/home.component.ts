162bd558d0be11b305dced47831b6f1f9c863582
Denver Huynh <dhuynh@DenverHuynh.local>
2017-04-30 00:59:36 -0400

    changed nav bar

--- denvr-web-ui/src/app/home.component.ts
+++ denvr-web-ui/src/app/home.component.ts
@@ -5,12 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: 'home.html',
   styleUrls: [
     '../assets/css/gallery.css',
-    '../assets/css/test.css',
     '../assets/css/nouislider.min.css',
-  ],
+    '../assets/css/test.css'
+  ]
 })
 export class homeComponent{
   lat: number = 39.7739;
   lng: number = -86.1763;
   someRange = [ 1, 100 ];
+
+  toggleMap() {
+
+  }
 }
